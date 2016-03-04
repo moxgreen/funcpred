d8eb954e271b0a6725ed6fd321ab56a224256c8b
Ivan Molineris <ivan.molineris@gmail.com>
2016-03-04 14:31:59 +0100

    html,css,js scaffolding

--- funcpred/urls.py
+++ funcpred/urls.py
@@ -1,9 +1,8 @@
 # -*- coding: utf-8 -*-
 from django.conf.urls import url
 
-from .views import RealtaListView,RealtaMarkersView
+from .views import index
 
 urlpatterns = [
-    url(r'^$', RealtaListView.as_view(), name='realta-list'),
-    url(r'^api/markers.tab$', RealtaMarkersView.as_view(), name='marker-list'),
+    url(r'^$', index, name='index'),
 ]
--- funcpred/views.py
+++ funcpred/views.py
@@ -1,40 +1,26 @@
 # Create your views here.
-from django.views.generic.list import ListView
 from django.utils import timezone
+from django.http import HttpResponse
 from django.shortcuts import render, get_object_or_404, redirect
-from django.contrib.auth.models import User, Group
 
-def my_view(request):
-        my_object = get_object_or_404(MyModel, pk=1)
+def index(request):
+    #return HttpResponse('Hello from Python!')
+    return render(request, 'index.html')
 
 
-from .models import Realta
-
-class RealtaListView(ListView):
-
-    model = Realta
-
-    def get_queryset(self):
-        #publisher = get_object_or_404(Publisher, name__iexact=self.args[0])
-        return Realta.objects.exclude(latitude=None)
-
-    def get_context_data(self, **kwargs):
-        context = super(RealtaListView, self).get_context_data(**kwargs)
-        return context
-
-class RealtaMarkersView(RealtaListView):
-    template_name="funcprod_app/realta_markers.tab"
-
-def redirect_user_to_realta_admin(request):
-    try:
-        realta = get_object_or_404(Realta, owner=request.user)
-        return redirect(realta.get_admin_url())
-    except Realta.MultipleObjectsReturned:
-        return redirect("/admin/funcprod_app/realta/")
-
-def homepage(request):
-    if request.user.is_staff:
-        return redirect("/admin/funcprod_app/realta/")
-    else:
-        return redirect("/funcprod_app/")
-        
+#from django.views.generic.list import ListView
+#class RealtaListView(ListView):
+#
+#    model = Realta
+#
+#    def get_queryset(self):
+#        #publisher = get_object_or_404(Publisher, name__iexact=self.args[0])
+#        return Realta.objects.exclude(latitude=None)
+#
+#    def get_context_data(self, **kwargs):
+#        context = super(RealtaListView, self).get_context_data(**kwargs)
+#        return context
+#
+#class RealtaMarkersView(RealtaListView):
+#    template_name="funcprod_app/realta_markers.tab"
+#
