78f49c42f2a88eca1d8438b6310716de70760eb6
Ivan Molineris <ivan.molineris@gmail.com>
2016-03-22 12:02:15 +0100

    H_CUTOFF

--- data/rules.mk
+++ data/rules.mk
@@ -1,6 +1,7 @@
 DATA_PRJ_ROOT=$(BIOINFO_ROOT)/prj/lncrna2function_clone/dataset/
-DATA_VERSION=gtex_single_tissue_log.GO.noIEA.BP
+DATA_VERSION?=gtex_single_tissue_log.GO.noIEA.BP
 MYSQL=mysql --local-infile -BCAN -u funcpred --password=funcpred -h 130.192.147.6 funcpred
+H_CUTOFF?=0.05
 
 db_GENES: /raid/molineri/bioinfotree/task/annotations/dataset/ensembl/hsapiens/73/gene-readable.map.gz
 	bawk '$$gene_id~/^ENS/ {if($$biotype!="protein_coding" && $$biotype!="pseudogene" && $$biotype!="lincRNA" && $$biotype!="antisense" && $$biotype!="antisense"){$$biotype="other"} \
@@ -9,12 +10,12 @@ db_GENES: /raid/molineri/bioinfotree/task/annotations/dataset/ensembl/hsapiens/7
 	| enumerate_rows > $@
 
 db_GeneFunction.$(DATA_VERSION).%: $(DATA_PRJ_ROOT)/$(DATA_VERSION)/%/all/predictions.fdr.gz db_ExpressionSource db_function db_GENES
-	bawk '$$H<0.05 {print $$H,$$GO,$$gene,$$leave_one_out}' $< \
+	bawk '$$H<$(H_CUTOFF) {print $$H,$$GO,$$gene,$$leave_one_out}' $< \
 	| translate <(bawk '{print $$GO,$$pk}' $^3) 2 \
 	| translate <(bawk '{print $$gene_id,$$pk}' $^4) 3 \
 	| append_each_row `bawk '$$4=="$*" {print $$1}' $^2` \
 	| bawk '{tmp=$$4; $$4=$$5; $$5=tmp; print}'\      *scambio due colonne per ordine in db 
-	| enumerate_rows -s `$(MYSQL)<<<"SELECT MAX(id) from funcpred_genefunction;"` > $@
+	| enumerate_rows -s $(shell tail -n 1 `ls -tr db_GeneFunction.* | grep -v LOADED | tail -n 1` | cut -f 1) > $@
 #echo "LOAD DATA LOCAL INFILE 'db_GENES' INTO TABLE funcpred_gene" | mysql --local-infile -u funcpred -p -h 130.192.147.6 funcpred
 
 db_GeneFunction.LOADED.%: db_GeneFunction.%
