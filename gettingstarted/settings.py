77270f49032954639fed2ea5559912710b5099c4
Ivan Molineris <ivan.molineris@gmail.com>
2016-03-23 11:07:17 +0100

    new database

--- gettingstarted/settings.py
+++ gettingstarted/settings.py
@@ -82,9 +82,9 @@ WSGI_APPLICATION = 'gettingstarted.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'funcpred',
-        'USER': 'root',
-        'PASSWORD': '***REMOVED***',
+        'NAME': 'funcpred2',
+        'USER': 'funcpred2',
+        'PASSWORD': 'funcpred',
         'HOST': '127.0.0.1',
         'PORT': '3306',
     }
